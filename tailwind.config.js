28398b381765541448922f723ed58232f67e5955
Chaoming Li <email@chaoming.li>
2024-11-15 14:00:15 +1100

    support saas tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@fireact.dev/core/dist/**/*.{js,mjs}"
+    "./node_modules/@fireact.dev/core/dist/**/*.{js,mjs}",
+    "./node_modules/@fireact.dev/saas/dist/**/*.{js,mjs}"
   ],
   theme: {
     extend: {},
