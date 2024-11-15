13e3ab3dac5adca655732e6b5fc9cfae53bed5ce
Chaoming Li <email@chaoming.li>
2024-11-15 14:06:14 +1100

    updated index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,30 @@
 import { initializeApp } from 'firebase-admin/app';
-import configFile from './saasConfig.json';
+
+// Initialize Firebase Admin
+initializeApp();
+
 import type { Plan, Permission } from '@fireact.dev/saas-cloud-functions';
+import configFile from './saasConfig.json';
+
+declare global {
+    var saasConfig: {
+        stripe: {
+            secret_api_key: string;
+            end_point_secret: string;
+        };
+        emulators: {
+            enabled: boolean;
+            useTestKeys: boolean;
+        };
+        plans: Plan[];
+        permissions: Record<string, Permission>;
+    };
+}
+
+// Initialize global config
+global.saasConfig = configFile;
+
+// Import cloud functions after config is initialized
 import {
   createSubscription,
   createInvite,
@@ -21,26 +45,6 @@ import {
   getBillingDetails,
   transferSubscriptionOwnership
 } from '@fireact.dev/saas-cloud-functions';
-
-// Initialize Firebase Admin
-initializeApp();
-
-// Set up global config
-declare global {
-    var saasConfig: {
-        stripe: {
-            secret_api_key: string;
-            end_point_secret: string;
-        };
-        emulators: {
-            enabled: boolean;
-            useTestKeys: boolean;
-        };
-        plans: Plan[];
-        permissions: Record<string, Permission>;
-    };
-}
-global.saasConfig = configFile;
 
 // Export cloud functions
 export {
